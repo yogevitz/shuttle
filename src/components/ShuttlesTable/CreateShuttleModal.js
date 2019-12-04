f1c81ae918dfdb9d22a9f3a41daa1a9f4c78b1a0
Yogev Shlomovitz <yogevshlomovitz@gmail.com>
2019-12-04 10:43:45 +0200

    feat: create shuttle form

--- src/components/ShuttlesTable/CreateShuttleModal.js
+++ src/components/ShuttlesTable/CreateShuttleModal.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import { MessageBoxFunctionalLayout } from 'wix-style-react/MessageBox';
 import Button from 'wix-style-react/Button';
+import FormField from 'wix-style-react/FormField';
+import Dropdown from 'wix-style-react/Dropdown';
+import Input from 'wix-style-react/Input';
 import Modal from 'wix-style-react/Modal';
 import { Add } from 'wix-style-react/new-icons';
+import { DESTINATIONS } from './ShuttlesTable';
 
 class CreateShuttleModal extends Component {
   constructor(props) {
@@ -12,10 +16,26 @@ class CreateShuttleModal extends Component {
     };
   }
 
+  getDestinationsDropDownArray() {
+    const destinations = [];
+    Object.keys(DESTINATIONS).forEach(function(key, index) {
+      destinations.push({
+        id: index,
+        value: DESTINATIONS[key],
+      });
+    });
+    return destinations;
+  }
+
+  divider() {
+    return <div style={{ height: '15px' }} />;
+  }
+
   render() {
     const setState = state => () => this.setState(state);
     const closeFullScreenModal = setState({ isOpenFullScreenModal: false });
     const openFullScreenModal = setState({ isOpenFullScreenModal: true });
+    const destinations = this.getDestinationsDropDownArray();
     return (
       <div>
         <Button
@@ -40,7 +60,30 @@ class CreateShuttleModal extends Component {
             theme="blue"
             title="Create Shuttle"
           >
-            Form
+            <div>
+              <FormField label={'Destination'} required>
+                <Dropdown
+                  placeholder="Select an option"
+                  options={destinations}
+                />
+              </FormField>
+              {this.divider()}
+              <FormField label={'Shuttle Name'} required>
+                <Input />
+              </FormField>
+              {this.divider()}
+              <FormField label={'Number of Seats'} required>
+                <Input />
+              </FormField>
+              {this.divider()}
+              <FormField label={'Contact Name'} required>
+                <Input />
+              </FormField>
+              {this.divider()}
+              <FormField label={'Contact Phone Number'} required>
+                <Input />
+              </FormField>
+            </div>
           </MessageBoxFunctionalLayout>
         </Modal>
       </div>
